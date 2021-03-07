942f50fd5c3d5ede0c650ef9032a501d1121260c
Dunebook <hyguyz@outlook.com>
2021-03-07 17:31:11 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import useOnline from './onlinestatus';
+
+import './App.css';
+
+function App() {
+
+
+  const online = useOnline();
+
+  return <div className="App">
+
+    <h3 class="ui center aligned icon header">
+
+      <i class="circular plug icon"></i>
+  React network Monitor
+
+</h3>
+
+    <div class="ui two column centered grid">
+
+      <span>
+        {online
+          ? <div className='ui green message'> <i class="wifi icon">  </i> Congrats you are ONLINE
+</div>
+          : <div className='ui red message'> <i class="meh icon">  </i> please check your network connection</div>
+        }
+      </span> </div>
+  </div>;
+}
+
+export default App;
